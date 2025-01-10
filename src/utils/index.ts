41bf9412ce6c78195f37c1c7f5a50990ea59aeec
SunburstMeoga <1025113986@qq.com>
2025-01-10 23:18:55 +0800

    feat

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,7 +23,7 @@ const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
   4: 'rinkeby.',
   5: 'goerli.',
   42: 'kovan.',
-  71204: '18.141.235.242:3001'
+  71204: 'hah'
 }
 
 export function getEtherscanLink(chainId: ChainId, data: string, type: 'transaction' | 'token' | 'address'): string {
